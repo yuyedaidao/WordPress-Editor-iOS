7d13f6aa81822b750d7f3aab63646f2e70fee0da
yuyedaidao <wyqpadding@gmail.com>
2020-02-06 15:15:40 +0800

    解决取消编辑会导致内容消失的问题

--- Classes/WPEditorView.m
+++ Classes/WPEditorView.m
@@ -1665,7 +1665,7 @@ shouldStartLoadWithRequest:(NSURLRequest *)request
 - (void)disableEditing
 {
     if (!self.sourceView.hidden) {
-        [self showVisualEditor];
+        //[self showVisualEditor];
     }
     
     [self.titleField disableEditing];
